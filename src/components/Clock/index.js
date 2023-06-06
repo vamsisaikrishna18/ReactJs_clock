1d28a0fddc1635b87b13a67a5d66418faac55e5d
vamsi <vamsiawesome319@gmail.com>
2023-06-06 11:27:19 +0000

    reactjs clock

---
+++ src/components/Clock/index.js
@@ -0,0 +1,36 @@
+import {Component} from 'react'
+import './index.css'
+
+class Clock extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {date: new Date()}
+    console.log('Constructor Called')
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount Called')
+    this.timerID = setInterval(this.tick, 1000)
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount Called')
+    clearInterval(this.timerID)
+  }
+
+  tick = () => {
+    this.setState({date: new Date()})
+  }
+
+  render() {
+    const {date} = this.state
+    console.log('Render Called', date)
+    return (
+      <div className="clock-container">
+        <h1 className="heading">Clock</h1>
+        <p className="time">{date.toLocaleTimeString()}</p>
+      </div>
+    )
+  }
+}
+export default Clock
